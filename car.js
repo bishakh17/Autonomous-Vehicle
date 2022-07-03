716ed8869f4f10eb84d045ca9a19019b1c088312
bishakh17 <71015419+bishakh17@users.noreply.github.com>
2022-07-03 16:53:02 +0530

    made the car mechanics more natural

--- car.js
+++ car.js
@@ -1,9 +1,16 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y){
         this.x=x;
         this.y=y;
-        this.width=width;
-        this.height=height;
+        this.width=30;
+        this.height=50;
+        
+        //gives the vehicle more natural motion
+        this.speed=0;
+        this.acceleration=0.4;
+        this.maxSpeed=8.5;
+        this.friction=0.06;
+        this.angle=0; //rotating car instead of going left and right is more natural
 
         this.controls=new Controls();
     }
@@ -13,25 +20,45 @@ class Car{
     }
 
     #move(){
-        if(this.controls.reverse){ 
-            this.y+=2;
-        }
+        //update and limit max speed
         if(this.controls.forward){
-            this.y-=2;
+            this.speed = Math.min(this.maxSpeed,this.speed+this.acceleration);
         }
-        if(this.controls.right){
-            this.x+=2;
+        if(this.controls.reverse){
+            this.speed = Math.max(-1*this.maxSpeed,this.speed-this.acceleration/1.7);
         }
-        if(this.controls.left){
-            this.x-=2;
+
+        //simulate road friction to slow down car
+        if(this.speed>0){
+            this.speed-=this.friction;
         }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+
+        //update car angle, if its going backwards turn in opposite direction
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        //update x and y coordinates of car based on calculated speed and direction(angle)
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
     draw(ctx){
         ctx.save();
-        
+
         //sets center of car
         ctx.translate(this.x,this.y); 
+        //rotate car
+        ctx.rotate(-this.angle);
         //draw rectangle around center
         ctx.beginPath();
         ctx.rect(                                  
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ canvas.width=200;
 canvas.height=window.innerHeight;
 
 const ctx = canvas.getContext("2d");
-const car=new Car(100,100,30,50);
+const car=new Car(100,100);
 
 animate();
 
